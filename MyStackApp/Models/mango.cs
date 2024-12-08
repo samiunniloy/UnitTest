5757b4232cf0dc2432069d76748bd546cdd8164d
Samiun Islam <60150274+samiunniloy@users.noreply.github.com>
2024-12-08 19:19:12 +0600

    Add files via upload

---
+++ MyStackApp/Models/mango.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Linq;
+
+namespace MyStackApp.Models
+{
+    public class mango
+    {
+     public   int? weight { get; set; }
+     public   string? isRipe {  get; set; }
+        public mango(int? Weight, string? IsRipe)
+        {
+            weight = Weight;
+           isRipe = IsRipe;            
+        }
+    }
+}
---
+++ MyStackApp/MyStackApp.cs
@@ -0,0 +1,50 @@
+﻿using MyStackApp.Models;
+using System.Dynamic;
+
+namespace MyStackApp
+{
+    public class MyStackApp<T>
+    {
+        private List<T> Items { get; set; } = new List<T>();
+        public bool IsEmpty()
+        {
+            return !Items.Any();
+        }
+
+        public void Push(T item)
+        {
+            Items.Add(item);
+        }
+
+        public int Size()
+        {
+            return Items.Count;
+        }
+        public async Task<T> Pop(bool flag)
+        {
+            
+            var obj = Items[Items.Count-1];
+            Items.RemoveAt(Items.Count - 1);
+            return obj;
+        }
+        public void Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Stack underflow");
+            }
+            Items.RemoveAt(Items.Count - 1);
+        }
+        public T Top()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Stack underflow");
+            }
+            var obj = Items[Items.Count - 1];
+            return obj;
+
+        }
+
+    }
+}
